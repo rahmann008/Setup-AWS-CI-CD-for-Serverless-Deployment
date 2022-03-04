6fc7395425b9e4ee5cc9027b2ac565e0aaeb9709
rahmann008 <arahmannali@gmail.com>
2022-03-04 14:47:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-    res.send('ok')
+    res.send('you made it')
   })
 app.get('/version', function (req, res) {
   res.send('Serverless ci/cd v2!')
